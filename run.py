c7bc7c8ff85c4ad53df6cedfedfc87889306c529
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-07 22:25:22 -0500

    tabla tarea

--- run.py
+++ run.py
@@ -9,6 +9,15 @@ db = SQLAlchemy()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db_todolist.db"
 db.init_app(app)
 
+class Tarea(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    descripcion = db.Column(db.String(200),nullable=False)
+    estado = db.Column(db.String(100),nullable=False)
+    
+    def __init__(self,descripcion,estado):
+        self.descripcion = descripcion
+        self.estado = estado
+
 db.create_all()
 
 @app.route('/')
