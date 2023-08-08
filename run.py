915da216f740c93fe5bad0900f0f9ad76714ab4e
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-08-07 22:08:45 -0500

    sql alchemy

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from flask import Flask,jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.app_context().push()
+
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db_todolist.db"
+db.init_app(app)
+
+db.create_all()
 
 @app.route('/')
 def index():
